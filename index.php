ab5e67472bd88b5a048928d42777c19b5702c65e
Ali Yaşar <43387306+aliyasarcomtr@users.noreply.github.com>
2020-04-18 22:29:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,25 +23,25 @@
     <form action="" method="post">
       <div class="row">
         <div class="col-md-6">
-          <input class="form-control" type="text" name="adsoyad" placeholder="Adı Soyadı"/>
+          <input class="form-control" type="text" name="Name" placeholder="Name"/>
         </div>
         <div class="col-md-6">
-          <input class="form-control" type="text" name="eposta" placeholder="Eposta"/>
+          <input class="form-control" type="text" name="Email" placeholder="Email"/>
         </div>
         <div class="col-md-6">
-          <input class="form-control" type="text" name="konu" placeholder="Konu"/>
+          <input class="form-control" type="text" name="Subject" placeholder="Subject"/>
         </div>
         <div class="col-md-6">
-          <input class="form-control" type="text" name="telefon" placeholder="Telefon"/>
+          <input class="form-control" type="text" name="Phone" placeholder="Phone"/>
         </div>
         <div class="col-md-12">
-          <textarea class="form-control" name="mesaj" placeholder="Mesaj"></textarea>
+          <textarea class="form-control" name="Message" placeholder="Message"></textarea>
         </div>
         <div class="col-md-12 mb-30">
           <div class="g-recaptcha" data-sitekey="6LdE4uoUAAAAAG7Cebvxl9HSrWDdEtAWspF08tHr"></div>
         </div>
         <div class="col-md-12 text-center-md-max mb-30">
-          <button type="submit" name="gonder" class="btn btn-size-2 btn-primary">Gonder</button>
+          <button type="submit" name="Send" class="btn btn-size-2 btn-primary">Send</button>
         </div>
       </div>
     </form>
@@ -50,7 +50,7 @@
       include 'class.phpmailer.php';
       include 'class.smtp.php';
 
-      if (isset($_POST['gonder'])) {
+      if (isset($_POST['Send'])) {
         $secretKey = "6LdE4uoUAAAAAO4kKULlbRBFnt5VMET_Yw44ioeL";
         $responseKey = $_POST['g-recaptcha-response'];
         $UserIP = $_SERVER['REMOTE_ADDR'];
@@ -62,48 +62,48 @@
         if ($response->success){
           $mail = new PHPMailer();
           $mail->IsSMTP();
-          $mail->Host     = "mail.aliyasar.com.tr";
+          $mail->Host     = "mail.site.com";
           $mail->SMTPAuth = true;
-          $mail->Username = "deneme@aliyasar.com.tr";
-          $mail->Password = "@1D*1c$4G";
+          $mail->Username = "email address";
+          $mail->Password = "password";
           $mail->CharSet = 'UTF-8';
           $mail->Port   = "587";
-          $mail->From     = "deneme@aliyasar.com.tr";
-          $mail->Fromname = "İletişim Formundan Gelen Mail";
-          $mail->AddAddress("aliyasarcomtr@gmail.com");
-          $mail->Subject  =  $_POST['konu'];
+          $mail->From     = "email address";
+          $mail->Fromname = "who mail";
+          $mail->AddAddress("send mail adres");
+          $mail->Subject  =  $_POST['subject'];
           $mail->IsHTML(true);
           $mail->Body     =  '
               <table>
                   <tr>
-                      <td style="border:1px solid #ddd;padding:5px">"Adı Soyadı"</td>
-                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['adsoyad']).'</td>
+                      <td style="border:1px solid #ddd;padding:5px">Name</td>
+                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['Name']).'</td>
                   </tr>
                   <tr>
-                      <td style="border:1px solid #ddd;padding:5px">"E-posta adresi"</td>
-                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['eposta']).'</td>
+                      <td style="border:1px solid #ddd;padding:5px">Email</td>
+                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['Email']).'</td>
                   </tr>
                   <tr>
-                      <td style="border:1px solid #ddd;padding:5px">"Telefon"</td>
-                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['telefon']).'</td>
+                      <td style="border:1px solid #ddd;padding:5px">Phone</td>
+                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['Phone']).'</td>
                   </tr>
                   <tr>
-                      <td style="border:1px solid #ddd;padding:5px">Konu</td>
-                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['konu']).'</td>
+                      <td style="border:1px solid #ddd;padding:5px">Subject</td>
+                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['Subject']).'</td>
                   </tr>
                   <tr>
-                      <td style="border:1px solid #ddd;padding:5px">Mesaj</td>
-                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['mesaj']).'</td>
+                      <td style="border:1px solid #ddd;padding:5px">Message</td>
+                      <td style="border:1px solid #ddd;padding:5px">'.urldecode($_POST['Message']).'</td>
                   </tr>
               </table>
           ';
           if (!$mail->Send()) {
-            echo "Mesajınız Gönderilemedi!" . $mail->ErrorInfo;
+            echo "Your message could not be sent!" . $mail->ErrorInfo;
             exit;
           }
-          echo "Mesajınız Gönderildi!";
+          echo "Your message has been sent!";
         }else{
-          echo "Doğrulamayı Geçemediniz!";
+          echo "You Didn't Pass Verification!";
         }
       }
     ?>
